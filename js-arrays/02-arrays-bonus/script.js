b750476d750ae4355e246b8a320805d1ea60bfc4
AlfredoF92 <alfred.fiorillo@gmail.com>
2025-04-16 17:22:37 +0200

    field: esercizio numero 2 svolto

--- js-arrays/02-arrays-bonus/script.js
+++ js-arrays/02-arrays-bonus/script.js
@@ -10,10 +10,45 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+// procedimento: 
+// reversedTeachers[0] = teachers[teachers.length-1];
+// reversedTeachers[1] = teachers[teachers.length-2];
+// reversedTeachers[2] = teachers[teachers.length-3];
+// reversedTeachers[3] = teachers[teachers.length-4];
+// reversedTeachers[4] = teachers[teachers.length-5];
+// reversedTeachers[5] = teachers[teachers.length-6];
+let reversedTeachers = [];
+y=0;
+for(i=1; i<=teachers.length; i++){ 
+  reversedTeachers[y]= teachers[teachers.length-i];
+  y++;
+  console.log("Nome da inserire: teacher[", i,  "] --> ", teachers[teachers.length-i] );
+  console.log("L'array reversedTeachers ora è --> ", y, reversedTeachers);
+}
+
+ console.log("#######################################################################");
+ let reversedTeachersPush = [];
+
+ for(i=1; i<=teachers.length; i++){ 
+  reversedTeachersPush.push(teachers[teachers.length-i]);
+  
+   console.log("Nome da inserire: teacher[", i,  "] --> ", teachers[teachers.length-i] );
+   console.log("L'array reversedTeachers ora è --> ", y, reversedTeachersPush);
+ }
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
-
+longNames = [];
+for(i=0; i<teachers.length; i++){
+  if(teachers[i].length >= 5){
+    longNames.push(teachers[i]);
+  }
+}
+console.log("L'array longNames ora è --> ", longNames); 
 // 3. Rimuovi 'Ed' dall'array teachers
+for(i=0; i<teachers.length; i++){
+  if(teachers[i]==="Ed"){
+    teachers.splice(i, 1);
+  }
+}
+console.log("L'array teachers senza ED è --> ", teachers); 
